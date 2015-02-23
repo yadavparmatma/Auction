71f56536ed8db0d4c09ba2832ba739c001265cd0
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-02-23 12:46:39 +0530

    home with admin login link ready and admin login page is designed.

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,35 @@ var auction = require("../own_modules/auctionLib.js").init("data/auction.db");
 var express = require('express');
 var router = express.Router();
 
+
+var loadUserFromSession = function(req,res,next){
+	req.session.userEmail && lib.getSingleUser(req.session.userEmail, function(err, user){
+		if(user){
+			var userInfo = {
+				id: user.id,
+				name: user.name,
+				email_id: user.email_id
+			};
+			req.user = userInfo;
+			res.locals.user = userInfo;
+		}else{
+			delete req.session.userEmail;
+		}
+	});
+	next();		
+};
+
+var requireLogin = function(req,res,next){
+	req.session.userEmail? next(): res.redirect('/login');
+};
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/Adminlogin',function(req,res){
+	res.render('Adminlogin');
 });
 
 module.exports = router;
