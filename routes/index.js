9cc925ac4ca4d356cb6fbd0beda3e51f4349e789
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-02-23 13:05:04 +0530

    land on dash board through admin login.

--- routes/index.js
+++ routes/index.js
@@ -27,17 +27,16 @@ var requireLogin = function(req,res,next){
 };
 
 /* GET home page. */
-// router.get('/', function(req, res) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
-router.get('/adminDashboard', function(req, res) {
-  res.render('adminDashboard');
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/Adminlogin',function(req,res){
-	res.render('Adminlogin');
+router.get('/adminLogin',function(req,res){
+	res.render('adminLogin');
+});
+
+router.post('/adminLogin',function(req,res){
+	res.render('adminDashboard');
 });
 
 module.exports = router;
