03099c94986b6da7141279e9d45421df164bec8f
vikaspal <vikaspal@thoughtworks.com>
2015-02-23 12:45:09 +0530

    adminDashboard created

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+router.get('/adminDashboard', function(req, res) {
+  res.render('adminDashboard');
 });
 
 module.exports = router;
