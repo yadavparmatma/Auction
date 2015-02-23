fab32fae92f696ad94762129a27c5e25c5d55eda
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-02-23 16:37:54 +0530

    for Auction master session is created.

--- own_modules/auctionLib.js
+++ own_modules/auctionLib.js
@@ -1,5 +1,50 @@
 var sqlite3 = require("sqlite3").verbose();
 
+var insertQueryMaker = function (tableName, data, fields) {
+	var columns = fields && ' (' + fields.join(', ') + ')' || '';
+	var values = '"' + data.join('", "') + '"';
+	var query = 'insert into ' + tableName + columns + ' values(' + values + ');';
+	return query;
+};
+
+var selectQueryMaker = function (tableName, retrivalData, where) {
+	retrivalData = retrivalData || ['*'];
+	var whatToGet = retrivalData.join(', ');
+	var whereToGet = where && retrieveWhereToGet(where) || '';
+
+	var query = 'select ' + whatToGet + ' from ' + tableName + whereToGet + ';';
+	return query;
+};
+
+var insertInto = function (db, fields, data, tableName, onComplete) {
+	var query = insertQueryMaker(tableName, data, fields);
+	db.run(query, onComplete);
+};
+
+
+var select = function (db, onComplete, tableName, retriveMethod, retrivalData, where) {
+	var query = selectQueryMaker(tableName, retrivalData, where);
+	db[retriveMethod](query, onComplete);
+};
+
+var retrieveWhereToGet = function (resource) {
+	var whereToGet = Object.keys(resource).map(function (key) {
+		return key + ' = "' + resource[key] + '"';
+	}).join(' and ');
+
+	return ' where ' + whereToGet;
+};
+
+var _getSingleUser = function(email_id,db,onComplete){
+	var whereToGet = {user_name: user_name};
+	select(db, onComplete, 'admin', 'get', null, whereToGet);
+};
+
+var _getPassword = function (user_name, db, onComplete) {
+	var whereToGet = {user_name: user_name};
+	select(db, onComplete, 'admin', 'get', ['password'], whereToGet);
+};
+
 var init = function(location){	
 	var operate = function(operation){
 		return function(){
@@ -18,8 +63,20 @@ var init = function(location){
 	};
 
 	var records = {
+		getPassword:operate(_getPassword)
 	};
 	return records;
 };
 
 exports.init = init;
+
+exports.queryParser = {
+	selectQueryMaker: selectQueryMaker,
+	insertQueryMaker: insertQueryMaker
+};
+
+
+exports.queryHandler = {
+	select: select,
+	insertInto: insertInto
+};
