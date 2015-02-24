ce571e7c7c96844fdaaffaaeae1a2730e787822a
parmatma <pyadav.it@gmail.com>
2015-02-24 22:52:48 +0530

    reform admin page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,14 +3,17 @@ var getItemId = function(){
 	var id = location[location.length-1];
 	return id.split('?')[0];
 }
-
+var showMessage = function(data){
+	alert(data);
+	$("#message").html(data);
+}
 
 var onPageLoad  = function(){
 	$("#register").click(function(){
 		var itemId = getItemId();
 		var email= $("#email").val();
 		var password = $("#password").val();
-		$.ajax({url:"/addToAuction/:"+itemId,type:"POST", dataType: "json",data:{email:email,password:password,itemId:itemId}});
+		$.ajax({url:"/addToAuction/:"+itemId,type:"POST", dataType: "json",data:{email:email,password:password,itemId:itemId}})
 	});
 };
 
