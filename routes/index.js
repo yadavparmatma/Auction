22eeb6ab066b020f4423c84abf9ddf5bbeba1f86
vishnumishra <vishnu143mishra@gmail.com>
2015-02-25 11:57:12 +0530

    refactore the function joinedAuction

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var express = require('express');
 var router = express.Router();
 
 
-
 var loadUserFromSession = function(req,res,next){
 	req.session.user_name && auction.getSingleUser(req.session.user_name, function(err, user){
 		if(user){
@@ -91,7 +90,7 @@ router.get('/adminDashboard',requireLogin,function(req,res){
 	res.render('adminDashboard');
 });
 
-router.get('/auction/:items_id',requireLoginForUser,function(req,res){
+router.get('/auction/:itemId',requireLoginForUser,function(req,res){
 	res.render('auction');
 });
 
@@ -158,6 +157,10 @@ router.get('/userDashboard',requireLoginForUser,function(req,res){
 	var items = {};
 	var id = req.session.user_id;
 	items.userName = req.session.name;
+	auction.getTopicsNameAndDate(function(err, topics){
+  		items.topics = topics;
+  	});
+
 	auction.getJoinedAuctions(id,function(err,joinedAuctionsDetails){
 		items.itemsDetails = joinedAuctionsDetails;
 		res.render('userDashboard',items);
